42ed4ea08a2e6d23a326ca025addfcaf31acd9d5
HARIHARAN B P <bphariharan1301@gmail.com>
2021-10-10 10:20:26 +0530

    Templates added

--- studymate/settings.py
+++ studymate/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Manually added apps
-    'base', 
+    'base',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,16 @@ ROOT_URLCONF = 'studymate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
+
+        # or in new version but, the above one can be used by default for any versions
+
+        # 'DIRS' : [
+        #     BASE_DIR / 'templates'
+        # ],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
