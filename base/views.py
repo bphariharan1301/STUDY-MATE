184336184675646e836d6e9610847917f3cb4bed
HARIHARAN B P <bphariharan1301@gmail.com>
2021-10-13 22:11:02 +0530

    Room Form created

--- base/views.py
+++ base/views.py
@@ -1,12 +1,41 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from base.models import Room
+from .forms import *
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html')
+    rooms = Room.objects.all().order_by('-updated', '-created')
+
+    context = {
+        'rooms': rooms,
+    }
+
+    return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+
+    context = {
+        'room': room,
+    }
+
+    return render(request, 'base/room.html', context)
+
+
+def create_room(request):
+    form = RoomForm
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'base/room_form.html', context)
--- studymate/settings.py
+++ studymate/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0_@=!&kz88(d@fpk(h&sym@mb_!ca9^r+#4(zog-il5wt7*=w_'
+# SECRET_KEY = 'django-insecure-0_@=!&kz88(d@fpk(h&sym@mb_!ca9^r+#4(zog-il5wt7*=w_'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -86,6 +93,12 @@ WSGI_APPLICATION = 'studymate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+'''DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
